f0d0a539db9286e8a62211da7ac6e10841ff87ab
ranjith-acharya <ranjithacharya997@gmail.com>
2023-01-06 01:26:46 +0530

    Updated Config App File for OpenAI API

--- config/app.php
+++ config/app.php
@@ -29,6 +29,7 @@ return [
     */
 
     'env' => env('APP_ENV', 'production'),
+    'openai_api_key' => env('OPENAI_API_KEY'),
 
     /*
     |--------------------------------------------------------------------------
