bd1b0f00d90aa01f82cef90eda15115292e0da60
ranjith-acharya <ranjithacharya997@gmail.com>
2023-01-06 01:24:55 +0530

    Created ArticleGenerator Controller

---
+++ app/Http/Controllers/ArticleGenerator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use OpenAI;
+
+class ArticleGenerator extends Controller
+{
+
+    public function index(Request $input)
+    {
+        if ($input->title == null) {
+            return;
+        }
+
+        $title = $input->title;
+
+        $client = OpenAI::client(config('app.openai_api_key'));
+
+        // $client = OpenAI::client(env('OPENAI_API_KEY'));
+        
+        $result = $client->completions()->create([
+            "model" => "text-davinci-003",
+            "temperature" => 0.7,
+            "top_p" => 1,
+            "frequency_penalty" => 0,
+            "presence_penalty" => 0,
+            'max_tokens' => 600,
+            'prompt' => sprintf('Write article about: %s', $title),
+        ]);
+
+        $content = trim($result['choices'][0]['text']);
+
+
+        return view('writer', compact('title', 'content'));
+    }
+
+}
