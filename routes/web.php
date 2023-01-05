c82d8012916084b833c8ab15e3f1dde236a021eb
ranjith-acharya <ranjithacharya997@gmail.com>
2023-01-06 01:26:03 +0530

    Update Web Application Routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleGenerator;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    $title = '';
+    $content = '';
+    return view('writer', compact('title', 'content'));
 });
+
+Route::post('/generate', [ArticleGenerator::class, 'index']);
